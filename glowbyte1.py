4e5ec10f25109106fffbcfea8c6a483275e46010
gavlique <gavlique@gmail.com>
2021-03-11 21:28:40 +0300

    Glowbyte take 1

---
+++ glowbyte1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+users = [
+{'id':1, 'name':'Vitaliy', 'age':20},
+{'id':2, 'name':'Alex', 'age':24},
+{'id':3, 'name':'Den', 'age':21}
+]
+
+phones = [
+{'id':1, 'user_id':1, 'number':"89009991011":
+
+
+users_df = pd.DataFrame(users)
+users_df =

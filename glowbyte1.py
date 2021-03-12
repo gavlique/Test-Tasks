33e53c025681af7fafbcb1a9b21f2572309196a3
gavlique <gavlique@gmail.com>
2021-03-12 09:51:53 +0300

    2nd

--- glowbyte1.py
+++ glowbyte1.py
@@ -6,8 +6,14 @@ users = [
 ]
 
 phones = [
-{'id':1, 'user_id':1, 'number':"89009991011":
+{'id':1, 'user_id':1, 'number':"89009991011"},
+{'id':2, 'user_id':1, 'number':"89002391231"},
+{'id':3, 'user_id':2, 'number':"88005553535"},
+{'id':4, 'user_id':3, 'number':"89045435335"}
 
 
 users_df = pd.DataFrame(users)
-users_df =
+phone_df = pd.DataFrame(phones
+merged = pd.merge(users_df, phones_df, how='left',
+                    left_on='id', right_on='user_id')
+merged
